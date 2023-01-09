c771cc506623ef08ef729c7dfa391c19be38948a
zgronska <zgronska@gmail.com>
2023-01-09 21:43:54 +0100

    Trying to make buttons work

--- script.js
+++ script.js
@@ -138,3 +138,31 @@ function slideshow() {
 }
 
 slideshow();
+
+// Order form close
+
+const formButton = document.querySelector("#form-toggle");
+const openFormButtons = document.querySelectorAll(".btn-open-form");
+const formEl = document.querySelector(".order-box");
+
+formButton.addEventListener("click", closeForm);
+openFormButtons.forEach((btn) => {
+  btn.addEventListener("click", openForm);
+});
+
+function closeForm() {
+  formEl.classList.remove("active");
+  formEl.classList.add("display-none");
+  console.log("close");
+}
+
+function openForm() {
+  if (formEl.classList.contains("active")) {
+    return;
+  } else {
+    formEl.classList.remove("display-none");
+    formEl.classList.add("active");
+  }
+
+  console.log("open");
+}
