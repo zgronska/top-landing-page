20654430be0632cea71e18a2ac7cdbd967234248
zgronska <zgronska@gmail.com>
2023-01-09 13:57:00 +0100

    Implemented responsive menu

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 // Sticky navbar
-let navbar = document.querySelector("nav");
-let main = document.querySelector("main");
+const navbar = document.querySelector("nav");
+const main = document.querySelector("main");
 
-let navPos = main.getBoundingClientRect().top;
+const mainPos = main.getBoundingClientRect().top;
 
 window.addEventListener("scroll", (e) => {
   let scrollPos = window.scrollY;
-  if (scrollPos > navPos - 1) {
+  if (scrollPos > mainPos - 10) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
+  }
+});
+
+// Responsive menu
+const toggleButton = document.querySelector("#btn-toggle");
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelectorAll(".nav-link a");
+
+function toggleMenu() {
+  menu.classList.toggle("active");
+  toggleButton.classList.toggle("fa-xmark");
+}
+
+toggleButton.addEventListener("click", toggleMenu);
+
+window.addEventListener("click", function (event) {
+  if (event.target !== menu && event.target !== toggleButton) {
+    menu.classList.remove("active");
   }
 });
