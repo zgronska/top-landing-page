cceebf48f480bd561574c2cf70c8a1c5c228f01f
zgronska <zgronska@gmail.com>
2023-01-09 14:43:26 +0100

    Added function to highlight links on scroll

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const mainPos = main.getBoundingClientRect().top;
 
 window.addEventListener("scroll", (e) => {
   let scrollPos = window.scrollY;
-  if (scrollPos > mainPos - 10) {
+  if (scrollPos >= mainPos) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
@@ -16,7 +16,6 @@ window.addEventListener("scroll", (e) => {
 // Responsive menu
 const toggleButton = document.querySelector("#btn-toggle");
 const menu = document.querySelector(".menu");
-const navLinks = document.querySelectorAll(".nav-link a");
 
 function toggleMenu() {
   menu.classList.toggle("active");
@@ -26,7 +25,45 @@ function toggleMenu() {
 toggleButton.addEventListener("click", toggleMenu);
 
 window.addEventListener("click", function (event) {
-  if (event.target !== menu && event.target !== toggleButton) {
+  if (event.target !== navbar && event.target !== toggleButton) {
     menu.classList.remove("active");
+    toggleButton.classList.remove("fa-xmark");
   }
 });
+
+// Update links on scroll
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    const makeActive = (link) =>
+      navLinks[link].classList.add("underline-active");
+    const removeActive = (link) =>
+      navLinks[link].classList.remove("underline-active");
+    const removeAllActive = () =>
+      [...Array(sections.length).keys()].forEach((link) => removeActive(link));
+
+    const sectionMargin = 200;
+    let currentActive = 0;
+
+    window.addEventListener("scroll", () => {
+      const current =
+        sections.length -
+        [...sections]
+          .reverse()
+          .findIndex(
+            (section) => window.scrollY >= section.offsetTop - sectionMargin
+          ) -
+        1;
+
+      if (current !== currentActive) {
+        removeAllActive();
+        currentActive = current;
+        makeActive(current);
+      }
+    });
+  },
+  false
+);
